cacb961cdc7d8e9500657d3b7549974e683cf7ff
Maksym Golik <maksym.golik@gmail.com>
2023-05-10 23:12:27 +0300

    Added entities: Role, Credential

---
+++ src/main/java/com/beecoders/ras/entity/Role.java
@@ -0,0 +1,38 @@
+package com.beecoders.ras.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Table(name = "roles")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(exclude = "credentials")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 45)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "role")
+    private List<Credential> credentials;
+}
