e2bdba787bbc766286876bb077273802cba42417
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-05-19 13:11:06 +0300

    RAS-79 Add constants in AuthConstant

--- src/main/java/com/beecoders/ras/model/constants/auth/AuthConstant.java
+++ src/main/java/com/beecoders/ras/model/constants/auth/AuthConstant.java
@@ -3,4 +3,5 @@ package com.beecoders.ras.model.constants.auth;
 public class AuthConstant {
     public static final String ACCOUNT_NOT_FOUND_ERROR_MESSAGE = "Account with username [%s] does not exist";
     public static final String INCORRECT_LOGIN_ERROR_MESSAGE = "Incorrect username or password, try again";
+    public static final String TABLE_ROLE = "TABLE";
 }
