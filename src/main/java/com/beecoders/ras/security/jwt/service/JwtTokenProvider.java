e5af92b94181eb85c3c6883e66756729e79a8c2f
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-05-19 21:45:40 +0300

    RAS-79 Add method for getting table name

--- src/main/java/com/beecoders/ras/security/jwt/service/JwtTokenProvider.java
+++ src/main/java/com/beecoders/ras/security/jwt/service/JwtTokenProvider.java
@@ -3,6 +3,8 @@ package com.beecoders.ras.security.jwt.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.beecoders.ras.model.entity.auth.Credential;
+import com.beecoders.ras.repository.RestaurantTableRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +15,12 @@ import static com.beecoders.ras.model.constants.auth.AuthConstant.TABLE_ROLE;
 import static com.beecoders.ras.security.jwt.constant.JwtTokenConstant.*;
 
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String secret;
+
+    private final RestaurantTableRepository restaurantTableRepository;
 
     public String generateJwtToken(Credential credential) {
         return JWT.create()
@@ -31,6 +36,10 @@ public class JwtTokenProvider {
 
     private String getName(Credential credential) {
         return (Objects.equals(credential.getRole().getName(), TABLE_ROLE))
-                ? credential.getTable().getName() : ADMIN_NAME;
+                ? getTableName(credential) : credential.getUsername();
+    }
+
+    private String getTableName(Credential credential) {
+        return restaurantTableRepository.findByCredential(credential).get().getName();
     }
 }
