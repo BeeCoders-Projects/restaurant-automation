a38d72b43e4c8250db4f86ef078420a20798b498
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-05-19 15:22:59 +0300

    RAS-78 Edit DishMapper

--- src/main/java/com/beecoders/ras/model/mapper/DishMapper.java
+++ src/main/java/com/beecoders/ras/model/mapper/DishMapper.java
@@ -1,12 +1,12 @@
 package com.beecoders.ras.model.mapper;
 
 import com.beecoders.ras.model.entity.Dish;
-import com.beecoders.ras.model.response.dish.DishInformation;
+import com.beecoders.ras.model.response.dish.DishInfo;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DishMapper  {
-    List<DishInformation> toDishInformations(List<Dish> dishes);
+    List<DishInfo> toDishInformations(List<Dish> dishes);
 }
