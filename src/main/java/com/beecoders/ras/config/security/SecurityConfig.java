580d6986b9543b650ad080d55fd19411567326a4
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-05-22 23:30:09 +0300

    Update SecurityConfig.java

--- src/main/java/com/beecoders/ras/config/security/SecurityConfig.java
+++ src/main/java/com/beecoders/ras/config/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                     corsConfiguration.addExposedHeader("Location");
                     corsConfiguration.addExposedHeader(AUTHORIZATION_HEADER);
                     corsConfiguration.addAllowedMethod(PATCH);
+                    corsConfiguration.addAllowedMethod(POST);
                     corsConfiguration.addAllowedMethod(PUT);
                     corsConfiguration.addAllowedMethod(DELETE);
                     return corsConfiguration;
