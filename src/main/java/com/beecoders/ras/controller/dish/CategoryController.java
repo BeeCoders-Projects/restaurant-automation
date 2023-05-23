ea172311d258aea87fe545fd40d8f8269e6eab8f
Maksym Golik <maksym.golik@gmail.com>
2023-05-24 00:39:16 +0300

    Added endpoint for category creation

--- src/main/java/com/beecoders/ras/controller/dish/CategoryController.java
+++ src/main/java/com/beecoders/ras/controller/dish/CategoryController.java
@@ -1,8 +1,10 @@
 package com.beecoders.ras.controller.dish;
 
+import com.beecoders.ras.model.request.dish.AddCategoryRequest;
 import com.beecoders.ras.model.response.dish.CategoryResponse;
 import com.beecoders.ras.service.dish.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -13,9 +15,15 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -49,4 +57,23 @@ public class CategoryController {
         return categoryService.getAllCategories();
     }
 
+    @SecurityRequirement(name = "bearerAuth")
+    @Operation(
+            summary = "Creating a category",
+            description = "I want to create a category for dishes.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "The category was created successfully"),
+            @ApiResponse(responseCode = "400", description = "Incorrect category data or image file",
+                    content = { @Content(schema = @Schema(implementation = String.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "401", description = "Log in to get access to the page",
+                    content = { @Content(schema = @Schema(implementation = String.class),
+                            mediaType = "application/json") })})
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CategoryResponse> save(@RequestPart AddCategoryRequest addCategoryRequest,
+                                                 @RequestPart MultipartFile image){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(categoryService.save(addCategoryRequest, image));
+    }
 }
