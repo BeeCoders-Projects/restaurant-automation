acaa121f62792aa762c29c99c3a45a4e94095243
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-05-24 10:10:20 +0300

    RAS-91 Create response model CategoryDetail

---
+++ src/main/java/com/beecoders/ras/model/response/dish/CategoryDetail.java
@@ -0,0 +1,11 @@
+package com.beecoders.ras.model.response.dish;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CategoryDetail {
+    private String name;
+    private String icon;
+}
