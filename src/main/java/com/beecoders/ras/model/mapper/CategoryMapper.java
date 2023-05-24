160b13459f70d280e6086b5d755f5c60ab9dff38
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-05-24 10:13:12 +0300

    RAS-91 Add method for mapping CategoryDetail

--- src/main/java/com/beecoders/ras/model/mapper/CategoryMapper.java
+++ src/main/java/com/beecoders/ras/model/mapper/CategoryMapper.java
@@ -1,11 +1,13 @@
 package com.beecoders.ras.model.mapper;
 
 import com.beecoders.ras.model.entity.Category;
+import com.beecoders.ras.model.response.dish.CategoryDetail;
 import com.beecoders.ras.model.response.dish.CategoryResponse;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
     CategoryResponse toCategoryResponse(Category category);
+    CategoryDetail toCategoryDetail(Category category);
 
 }
