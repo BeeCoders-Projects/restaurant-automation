5a35b5da82111cf66c84505a4529da634aaa0baa
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-06-06 19:36:08 +0300

    RAS-130 Add error message in AuthConstant

--- src/main/java/com/beecoders/ras/model/constants/AuthConstant.java
+++ src/main/java/com/beecoders/ras/model/constants/AuthConstant.java
@@ -3,5 +3,6 @@ package com.beecoders.ras.model.constants;
 public class AuthConstant {
     public static final String ACCOUNT_NOT_FOUND_ERROR_MESSAGE = "Account with username [%s] does not exist";
     public static final String INCORRECT_LOGIN_ERROR_MESSAGE = "Incorrect username or password, try again";
+    public static final String ALREADY_LOGIN_ERROR_MESSAGE = "The account with username already authorized, try later";
     public static final String TABLE_ROLE = "TABLE";
 }
