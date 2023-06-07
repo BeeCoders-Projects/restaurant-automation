08a1c3fd1238a473318e816b3cf0f8f8ce05a965
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-06-07 19:33:09 +0300

    RAS-134 Create constants for restaurant table

---
+++ src/main/java/com/beecoders/ras/model/constants/RestaurantTableConstant.java
@@ -0,0 +1,7 @@
+package com.beecoders.ras.model.constants;
+
+public class RestaurantTableConstant {
+    public static final String TABLE_NOT_FOUND_ERROR_MESSAGE = "Table was not found";
+    public static final String STATUS_TABLE_NOT_FOUND_ERROR_MESSAGE = "Status of table was not found";
+    public static final String CHANGE_OTHER_TABLE_ERROR_MESSAGE = "Illegal operation to changing status other table";
+}
