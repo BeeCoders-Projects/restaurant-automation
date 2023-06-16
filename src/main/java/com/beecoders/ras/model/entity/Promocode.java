8f0241bfce10febdb87407acd0047755e2d21588
Dmytro Teliukov <77294463+DmytroTeliukov@users.noreply.github.com>
2023-06-16 12:25:09 +0300

    RAS-167 Create entity Promocode and link to Order (#38)
    * RAS-167 Create entity Promocode and link to Order
    
    * RAS-167 Add uq-constraint for field code in Promocode

---
+++ src/main/java/com/beecoders/ras/model/entity/Promocode.java
@@ -0,0 +1,43 @@
+package com.beecoders.ras.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Table(name = "promocodes")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Builder
+public class Promocode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "from_date", nullable = false)
+    private LocalDate fromDate;
+
+    @Column(name = "to_date", nullable = false)
+    private LocalDate toDate;
+
+    @Column(name = "holder_name", nullable = false)
+    private String holderName;
+
+    @Min(0)
+    @Max(100)
+    @Column(name = "discount_percent", nullable = false)
+    private Byte discountPercent;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "promocode")
+    private List<Order> orders;
+}
