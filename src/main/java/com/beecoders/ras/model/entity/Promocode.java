cdafbe70dd939b7a779674e30f9a78f302f101d7
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-06-16 12:10:42 +0300

    RAS-167 Add uq-constraint for field code in Promocode

--- src/main/java/com/beecoders/ras/model/entity/Promocode.java
+++ src/main/java/com/beecoders/ras/model/entity/Promocode.java
@@ -21,7 +21,7 @@ public class Promocode {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String code;
 
     @Column(name = "from_date", nullable = false)
